14751803ca71cbd364be7e806695da4e1c8c959e
Kayce <pat.nzediegwu@gmail.com>
2024-08-27 15:53:08 -0700

    feat: nav

--- src/lib/layout/Nav.tsx
+++ src/lib/layout/Nav.tsx
@@ -4,7 +4,7 @@ import ThemeToggle from './ThemeToggle';
 
 const Nav = () => {
   return (
-    <Flex as="nav" width="full" p="1em" h="5em" justifyContent="right">
+    <Flex as="nav" width="100%" p="1em" h="5em" justifyContent="right">
       <ThemeToggle />
     </Flex>
   );
