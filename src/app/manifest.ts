fc094563fe04ba317bee9d3d721844e4957199fb
Kayce <pat.nzediegwu@gmail.com>
2024-08-27 14:02:24 -0700

    chore: favicon

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -1,18 +1,18 @@
 import type { MetadataRoute } from 'next';
 
 const manifest = (): MetadataRoute.Manifest => ({
-  short_name: 'nextarter-chakra',
-  name: 'Next.js App Template',
+  short_name: 'Patrick Nzediegwu',
+  name: 'Patrick Nzediegwu',
   lang: 'en',
   start_url: '/',
-  background_color: '#FFFFFF',
+  background_color: '#05051e',
   theme_color: '#FFFFFF',
   dir: 'ltr',
   display: 'standalone',
   prefer_related_applications: false,
   icons: [
     {
-      src: '/vercel.svg',
+      src: '/favicon.ico',
       purpose: 'any',
       sizes: '48x48 72x72 96x96 128x128 256x256',
     },
