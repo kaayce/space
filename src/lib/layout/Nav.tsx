d1cbaf798329a0ca580d90e8ae336df7dd8fd8d6
Kayce <pat.nzediegwu@gmail.com>
2024-08-27 15:44:03 -0700

    fix: adjust nav

--- src/lib/layout/Nav.tsx
+++ src/lib/layout/Nav.tsx
@@ -1,13 +1,11 @@
-import { Box, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
 
 import ThemeToggle from './ThemeToggle';
 
 const Nav = () => {
   return (
-    <Flex as="nav" width="full" p="1em" h="5em" textAlign="right">
-      <Box marginLeft="auto">
-        <ThemeToggle />
-      </Box>
+    <Flex as="nav" width="full" p="1em" h="5em" justifyContent="right">
+      <ThemeToggle />
     </Flex>
   );
 };
