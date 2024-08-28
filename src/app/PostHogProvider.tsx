419b1c3716d0038e98bb5409f83aa9ee5318f63a
Kayce <pat.nzediegwu@gmail.com>
2024-08-28 13:40:21 -0700

    feat: add posthog analytics

---
+++ src/app/PostHogProvider.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import posthog from 'posthog-js';
+import { PostHogProvider } from 'posthog-js/react';
+
+if (typeof window !== 'undefined') {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+    person_profiles: 'identified_only', // or 'always' to create profiles for anonymous users as well
+  });
+}
+
+interface CSPostHogProviderProps {
+  children: React.ReactNode;
+}
+
+function CSPostHogProvider({ children }: CSPostHogProviderProps) {
+  return <PostHogProvider client={posthog}>{children}</PostHogProvider>;
+}
+
+export default CSPostHogProvider;
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,13 +2,16 @@
 
 import { CacheProvider } from '@chakra-ui/next-js';
 
+import CSPostHogProvider from '~/app/PostHogProvider';
 import { Chakra as ChakraProvider } from '~/lib/components/Chakra';
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <CacheProvider>
-      <ChakraProvider>{children}</ChakraProvider>
-    </CacheProvider>
+    <CSPostHogProvider>
+      <CacheProvider>
+        <ChakraProvider>{children}</ChakraProvider>
+      </CacheProvider>
+    </CSPostHogProvider>
   );
 };
 
