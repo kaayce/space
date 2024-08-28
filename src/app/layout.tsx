82cb60b29bf242681045f06446f04e32ab9537d6
Kayce <pat.nzediegwu@gmail.com>
2024-08-28 11:07:36 -0700

    fix: update viewport

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import { CSSReset } from '@chakra-ui/react';
-import type { Metadata, Viewport } from 'next';
+import type { Metadata } from 'next';
 
 import { inter } from '~/app/fonts';
 import Providers from '~/app/providers';
@@ -36,13 +36,14 @@ export const metadata: Metadata = {
     creator: '@zenandvibes',
     card: 'summary_large_image',
   },
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1',
 };
 
-export const viewport: Viewport = {
-  width: 'device-width',
-  initialScale: 0,
-  themeColor: '#FFFFFF',
-};
+// export const viewport: Viewport = {
+//   width: 'device-width',
+// initialScale: 1,
+//   themeColor: '#FFFFFF',
+// };
 
 const RootLayout = ({ children }: RootLayoutProps) => {
   return (
